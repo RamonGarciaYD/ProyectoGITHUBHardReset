a22f7cd34f33b4a97dc48885e5095c9514867754
Ramon <159023662+RamonGarciaYD@users.noreply.github.com>
2024-03-23 16:26:52 +0100

    Añadido int num = 2
    Hemos añadido una variable int

--- Saludos.java
+++ Saludos.java
@@ -3,4 +3,4 @@ System.out.println("Como estas?");
 System.out.println("Hace mucho que no te veo");
 System.out.println("Que alegria verte");
 System.out.println("A ver si hablamos mas");
-System.out.println("A ver si quedamos esta semana");
+int num = 2
