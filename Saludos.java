e51d13c8bfe8ba691c9c061733a4b6cbb8f28d92
Ramon <159023662+RamonGarciaYD@users.noreply.github.com>
2024-03-23 16:19:02 +0100

    Tenemos que vernos mas sustituido por: a ver si hablamos mas
    He sustituido el saludo: tenemos que ver nos mas por a ver si hablamos mas

--- Saludos.java
+++ Saludos.java
@@ -2,5 +2,5 @@ System.out.println("Hola");
 System.out.println("Como estas?");
 System.out.println("Hace mucho que no te veo");
 System.out.println("Que alegria verte");
-System.out.println("Tenemos que vernos mas");
+System.out.println("A ver si hablamos mas");
 System.out.println("A ver si quedamos");
