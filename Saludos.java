6e8a8803e90fc1a6542a7ced9fcb9098572236b4
Ramon <159023662+RamonGarciaYD@users.noreply.github.com>
2024-03-23 16:29:48 +0100

    Borrado para merge

--- Saludos.java
+++ Saludos.java
@@ -1,7 +1,5 @@
 System.out.println("Hola");
 System.out.println("Como estas?");
 System.out.println("Hace mucho que no te veo");
-System.out.println("Que alegria verte");
-System.out.println("A ver si hablamos mas");
 int num = 2
 int num2 = 20
